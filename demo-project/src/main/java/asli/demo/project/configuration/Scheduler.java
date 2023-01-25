03ad75e3c0326860a13a8c9d65d6e521b3e223bc
Aslıhan Sönmez <73489259+Aslihan0@users.noreply.github.com>
2023-01-25 12:36:41 +0300

    Add files via upload
    "demo project"

---
+++ demo-project/src/main/java/asli/demo/project/configuration/Scheduler.java
@@ -0,0 +1,21 @@
+package asli.demo.project.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+    @Scheduled(cron = "* * * * * ?")
+    public void cronJobSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("hello asli:: " + strDate);
+    }
+
+    }
+
